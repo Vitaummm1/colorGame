c81874bfe1e5ceadf4caaf5322b812dd70ed5ebf
Vitaummm1 <keyke@hotmail.com.br>
2022-02-08 16:21:09 -0300

    reduction of lines of code

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-const randParam = () => {
-    return Math.floor(Math.random()*256);}
-
-const difColor = () => {
-    return Math.floor(Math.random()*9);
-}
-
-const colors = document.querySelectorAll('.color');
-const startButton = document.querySelector('button');
-let highScoreStatus = document.querySelector('#highScore');
-let scoreStatus = document.querySelector('#score');
+// definition of constants
+const colors = document.querySelectorAll(".color");
+const startButton = document.querySelector("button");
+let highScoreStatus = document.querySelector("#highScore");
+let scoreStatus = document.querySelector("#score");
 let highScore = 0;
 let score = 0;
 highScoreStatus.innerHTML = highScore;
 scoreStatus.innerHTML = score;
-
-for(let color of colors){
-    color.style.backgroundColor = 'white';
+// initial events
+for (let color of colors) {
+    color.style.backgroundColor = "white";
 }
+// definition of functions
+const randParam = () => {
+    return Math.floor(Math.random() * 256);
+};
+
+const difColor = () => {
+    return Math.floor(Math.random() * 9);
+};
+
+const scoreColor = function (r, g, b, limiter) {
+    r -= Math.floor(Math.random() * limiter);
+    if (r < 0) {
+        r = 0;
+    }
+    g -= Math.floor(Math.random() * limiter);
+    if (g < 0) {
+        g = 0;
+    }
+    b -= Math.floor(Math.random() * limiter);
+    if (b < 0) {
+        b = 0;
+    }
+    return `rgb(${r},${g},${b})`;
+};
 
 const gameStart = () => {
     let r = randParam();
@@ -24,83 +42,45 @@ const gameStart = () => {
     let b = randParam();
     let gameColor1 = `rgb(${r},${g},${b})`;
     let difNumber = parseInt(difColor());
-    for (let color of colors){
+    for (let color of colors) {
         color.style.backgroundColor = gameColor1;
     }
-    colors[difNumber].classList.toggle('wrongButton');
-    if(score<=5){
-        r -= Math.floor(Math.random()*80);
-        if(r<0){
-        r=0;
-        }
-        g -= Math.floor(Math.random()*80);
-        if(g<0){
-        g=0;
-        }
-        b -= Math.floor(Math.random()*80);
-        if(b<0){
-        b=0;
-        }
-    gameColor2 = `rgb(${r},${g},${b})`;
+    colors[difNumber].classList.toggle("wrongButton");
+    if (score <= 5) {
+        gameColor2 = scoreColor(r, g, b, 80);
+    } else if (score > 5 && score <= 10) {
+        gameColor2 = scoreColor(r, g, b, 50);
+    } else {
+        gameColor2 = scoreColor(r, g, b, 20);
     }
-    else if (score>5&&score<=10){
-        r -= Math.floor(Math.random()*50);
-        if(r<0){
-        r=0
-        }
-        g -= Math.floor(Math.random()*50);
-        if(g<0){
-        g=0
-        }
-        b -= Math.floor(Math.random()*50);
-        if(b<0){
-        b=0
-        }
-    gameColor2 = `rgb(${r},${g},${b})`;
-    }
-    else {
-        r -= Math.floor(Math.random()*20);
-        if(r<0){
-        r=0;
-        }
-        g -= Math.floor(Math.random()*20);
-        if(g<0){
-        g=0;
-        }
-        b -= Math.floor(Math.random()*20);
-        if(b<0){
-        b=0;
-        }
-    gameColor2 = `rgb(${r},${g},${b})`;
-    }
-    document.querySelector('.wrongButton').style.backgroundColor = gameColor2;
+    document.querySelector(".wrongButton").style.backgroundColor = gameColor2;
     startButton.disabled = true;
-}
+};
 
 const gameEnd = () => {
-    for(let color of colors){
-        color.style.backgroundColor = 'white';
+    for (let color of colors) {
+        color.style.backgroundColor = "white";
     }
-    score=0;
-    scoreStatus.innerHTML=score;
+    score = 0;
+    scoreStatus.innerHTML = score;
     startButton.disabled = false;
-}
-startButton.addEventListener('click', gameStart);
+};
+// definition of events
+startButton.addEventListener("click", gameStart);
 
-for(let color of colors){
-    color.addEventListener('click', () => {
-        if(color.classList.contains('wrongButton') === true){
-        color.classList.toggle('wrongButton');
-        gameStart();
-        score++;
-        scoreStatus.innerHTML = score;
-        if(highScore<score){
-            highScore=score;
-            highScoreStatus.innerHTML = highScore;
+for (let color of colors) {
+    color.addEventListener("click", () => {
+        if (color.classList.contains("wrongButton") === true) {
+            color.classList.toggle("wrongButton");
+            gameStart();
+            score++;
+            scoreStatus.innerHTML = score;
+            if (highScore < score) {
+                highScore = score;
+                highScoreStatus.innerHTML = highScore;
+            }
+        } else {
+            gameEnd();
         }
-        }
-        else{
-        gameEnd();
-        }
-    })
+    });
 }
